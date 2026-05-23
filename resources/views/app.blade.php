7fbb0310ba0e1c0108d414a3c9eb9799440e7443
Dimitar <dvitanov@bitdms.com>
2026-05-23 10:05:29 +0200

    Handling favi icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title inertia>{{ config('app.name', 'Limitless Auto Detailing') }}</title>
+        <title inertia>{{ config('app.name', 'MDetailing Premium Car Care') }}</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:300,400,500,600,700,800,900|playfair-display:400,500,600,700,800,900" rel="stylesheet" />
         @vite(['resources/js/app.js', 'resources/css/app.css'])
